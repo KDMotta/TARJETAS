29301a1bc8b10b77d7df7de89a91f368ff216934
Daniela Motta <79612599+KDMotta@users.noreply.github.com>
2022-08-04 08:08:51 -0600

    Se completó el ejercicio

---
+++ js/data.js
@@ -0,0 +1,5 @@
+//DATA
+export let grupoTarjetas1 = ["🍌", "🍉", "🍇", "🍓", "🫐", "🍑", "🍍", "🥝", "🥭"]
+export let grupoTarjetas2 = ["🍌", "🍉", "🍇", "🍓", "🫐", "🍑", "🍍", "🥝", "🥭"]
+
+export let TodasLasTarjetas = grupoTarjetas1.concat(grupoTarjetas2);
